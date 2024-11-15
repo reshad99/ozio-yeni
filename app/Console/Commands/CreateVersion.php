f848d18c2478c45a5589c8834250f024386c2623
Rashad Mammadov <Rashad.Mammadov@venta.az>
2024-11-15 15:23:41 +0400

    Versioning finish

---
+++ app/Console/Commands/CreateVersion.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+class CreateVersion extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:version  {newVersion} {oldVersion=v1}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Copy controllers, requests, and resources to new version folder';
+
+    private $appDirectories = [
+        'Http/Controllers',
+        'Http/Resources',
+        'Http/Requests',
+        'Services',
+    ];
+
+    private $rootDirectories = [
+        'routes',
+    ];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $filesystem = new Filesystem();
+        $newVersion = ucfirst($this->argument('newVersion'));
+        $oldVersion = ucfirst($this->argument('oldVersion'));
+
+        // copy directories under 'app' directory
+        foreach ($this->appDirectories as $directory) {
+            $source = app_path($directory . '/' . $oldVersion);
+            $destination = app_path($directory . '/' . $newVersion);
+            if (!$filesystem->exists($source)) {
+                $this->error('Source directory does not exist : ' . $source);
+                return 1;
+            }
+
+            try {
+                $filesystem->mirror($source, $destination);
+                $this->replaceNamespace($destination, $oldVersion, $newVersion);
+                $this->info('Directory has been copied : ' . $destination);
+            } catch (IOExceptionInterface $exception) {
+                $this->error('An error occurred while creating your directory at ' . $exception->getPath());
+                return 1;
+            }
+        }
+
+        // copy directories under root directory
+        foreach ($this->rootDirectories as $directory) {
+            $source = base_path($directory . '/' . $oldVersion);
+            $destination = base_path($directory . '/' . $newVersion);
+            if (!$filesystem->exists($source)) {
+                $this->error('Source directory does not exist : ' . $source);
+                return 1;
+            }
+
+            try {
+                $filesystem->mirror($source, $destination);
+                $this->replaceNamespace($destination, $oldVersion, $newVersion);
+                $this->info('Directory has been copied : ' . $destination);
+            } catch (IOExceptionInterface $exception) {
+                $this->error('An error occurred while creating your directory at ' . $exception->getPath());
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+
+    private function replaceNamespace($path, $oldVersion, $newVersion)
+    {
+        $items = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($items as $item) {
+            if ($item->isFile() && $item->getExtension() === 'php') {
+                $fileContents = file_get_contents($item->getRealPath());
+                $fileContents = str_replace($oldVersion, $newVersion, $fileContents);
+                file_put_contents($item->getRealPath(), $fileContents);
+            }
+        }
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,28 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api_v1.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+
+            foreach (config('services.versions') as $version) {
+                $version = ucfirst($version);
+
+                Route::middleware('api')
+                ->prefix("api/$version")
+                ->group(base_path('routes/api.php'));
+            }
+
+
+                Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
