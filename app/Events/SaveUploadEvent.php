22831a7a30d21df7f52b58ef8976e2633cb2c909
NejatJafarli <mega.ceferli@gmail.com>
2024-11-20 16:23:20 +0400

    Composer analyse fix

--- app/Events/SaveUploadEvent.php
+++ app/Events/SaveUploadEvent.php
@@ -4,7 +4,9 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
 
 class SaveUploadEvent
 {
@@ -12,20 +14,45 @@ class SaveUploadEvent
     use InteractsWithSockets;
     use SerializesModels;
 
+    /**
+     * Create a new event instance.
+     * @var UploadedFile $file
+     */
     public $file;
+    /**
+     *  @var string $model
+     */
     public $model;
+    /**
+     *  @var ?string $type
+     */
     public $type;
+    /**
+     *  @var bool $overwrite
+     */
     public $overwrite;
+    /**
+     *  @var ?int $uploadId
+     */
     public $uploadId;
+    /**
+     *  @var string $filePath
+     */
     public $filePath = null;
 
     /**
      * Create a new event instance.
-     *
+     * 
+     * @param UploadedFile $file
+     * @param string $model
+     * @param ?string $type
+     * @param bool $overwrite
+     * @param ?int $uploadId
      * @return void
      */
     public function __construct($file, $model, $type = null, $overwrite = false, $uploadId = null)
     {
+
         $this->file = $file;
         $this->model = $model;
         $this->type = $type;
