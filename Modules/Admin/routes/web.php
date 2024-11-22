d17e944afe4129ad1d3dbffe0e6de09fcf62c74f
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-22 15:50:36 +0400

    add-guest-middleware

--- Modules/Admin/routes/web.php
+++ Modules/Admin/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Admin\Http\Controllers\AdminController;
 use Modules\Admin\Http\Controllers\Auth\LoginController;
+use Modules\Admin\Http\Controllers\Auth\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,11 @@ Route::group([], function () {
     Route::resource('admin', AdminController::class)->names('admin');
 });
 
-Route::get('login', [LoginController::class, 'showLogin'])->name('login');
-Route::post('authenticate', [LoginController::class, 'authenticate'])->name('admin.authenticate');
+Route::middleware('guest:admin')->group(function () {
+    Route::get('login', [LoginController::class, 'showLogin'])->name('login');
+    Route::post('authenticate', [LoginController::class, 'authenticate'])->name('admin.authenticate');
+});
+
 Route::post('logout', [LogoutController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:admin')->group(function () {
