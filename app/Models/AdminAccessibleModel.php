0dd3ed05a33cc98a5139b34f8a625540453ce038
XediceYusifova <yxedice1@gmail.com>
2024-11-25 15:05:44 +0400

    add models and repositories

--- app/Models/AdminAccessibleModel.php
+++ app/Models/AdminAccessibleModel.php
@@ -3,8 +3,33 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class AdminAccessibleModel extends Model
 {
-    //
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array<string>|bool
+     */
+    protected $guarded = [];
+
+    /**
+     * @return BelongsTo<Admin, self>
+     */
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(Admin::class);
+    }
+
+    /**
+     * Get the accessible model (morph relation).
+     *
+     * @return MorphTo
+     */
+    public function accessible(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
