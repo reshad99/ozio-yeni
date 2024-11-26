49f6c9b4845fe5b8f98b20c5dbecb81a77b099c3
NejatJafarli <mega.ceferli@gmail.com>
2024-11-26 10:54:24 +0400

    update repository classes to use V1 namespace for consistency

--- app/Repositories/Concrete/V1/SmsLogRepository.php
+++ app/Repositories/Concrete/V1/SmsLogRepository.php
@@ -2,6 +2,6 @@
 
 namespace App\Repositories\Concrete\V1;
 
-use App\Repositories\Concrete\BaseRepository;
+use App\Repositories\Concrete\V1\BaseRepository;
 
 class SmsLogRepository extends BaseRepository {}
