7f749ece2e09142b398ccaa189085db7bf8bda87
khanlarazizov <xanlar.ezizov18@gmail.com>
2024-11-27 11:58:10 +0400

    spatie-permission-added

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,17 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        //        User::factory()->create([
-        //            'name' => 'Test User',
-        //            'email' => 'test@example.com',
-        //        ]);
-
-        Admin::create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('admin123'),
-        ]);
+        $this->call(
+            [
+                RoleAndPermissionSeeder::class,
+                AdminSeeder::class,
+            ]
+        );
     }
 }
