f45bc6e76354bc39eb643e0425cc93032ac1f7c7
Rashad Mammadov <Rashad.Mammadov@venta.az>
2024-12-02 15:12:46 +0400

    fix

--- routes/V1/api.php
+++ routes/V1/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(middleware: 'guest')->group(callback: function (): void {
+Route::middleware(middleware: 'guest:api')->group(callback: function (): void {
 });
