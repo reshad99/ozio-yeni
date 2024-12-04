6592aa7c6cf0e94064a42afddddbe15ff8274873
NejatJafarli <mega.ceferli@gmail.com>
2024-12-04 13:33:17 +0400

    Refactor edit modal script; streamline AJAX calls, improve variable naming, and remove commented-out code

--- Modules/Admin/resources/views/pages/users/list/scripts/edit-modal.blade.php
+++ Modules/Admin/resources/views/pages/users/list/scripts/edit-modal.blade.php
@@ -1,30 +1,38 @@
 <script>
     $(document).on('click', "a[data-bs-target='#kt_modal_new2_target']", async function() {
         // <input class="form-check-input" type="checkbox" value="${data}" /> get value of checkbox
-        KTApp.showPageLoading();
+        await window.loadAjax();
 
-        await window.LoadSelectAddressModalAjax();
-
-        let id = $(this).data('id');
+        let user_id = $(this).data('id');
 
         let form = $("#kt_modal_new2_target_form");
-        //TODO ROUTE
-        let url = `{{ route('', ['id' => '-1']) }}`;
+        //ROUTE USER
+        let url =
+            `{{ route('', ['id' => '-1']) }}`;
+        url = url.replace("-1", user_id);
+        KTApp.showPageLoading();
+
         //ajax read role data
         $.ajax({
             type: "GET",
-            data: {},
-            url: url.replace("-1", id), // Update with your API endpoint
+            url: url, // Update with your API endpoint
+            data: {
+
+            },
             success: function(response) {
-                // Handle the success response here
                 console.log(response);
-                form.find("input[name='edit_id']")
-                    .val(response.id);
+                //clear form all inputs
+                form.find("input").val("");
+
+
+                //set form data
+                form.find("input[name='edit_id']").val(response.id);
                 KTApp.hidePageLoading();
+
             },
             error: function(xhr, status, error) {
-                // Handle errors here
                 KTApp.hidePageLoading();
+                // Handle errors here
                 customSwal.dataError(xhr);
             },
         });
@@ -37,7 +45,6 @@
         // Class definition
         var KTModalNewTarget = function() {
             var submitButton;
-            var closeButton;
             var cancelButton;
             var validator;
             var form;
@@ -46,8 +53,33 @@
 
             // Init form inputs
             var initForm = function() {
-                // Tags. For more info, please visit the official plugin site: https://yaireo.github.io/tagify/
+                // // Tags. For more info, please visit the official plugin site: https://yaireo.github.io/tagify/
+                // var tags = new Tagify(form.querySelector('[name="tags"]'), {
+                //     whitelist: ["Important", "Urgent", "High", "Medium", "Low"],
+                //     maxTags: 5,
+                //     dropdown: {
+                //         maxItems: 10, // <- mixumum allowed rendered suggestions
+                //         enabled: 0, // <- show suggestions on focus
+                //         closeOnSelect: false // <- do not hide the suggestions dropdown once an item has been selected
+                //     }
+                // });
+                // tags.on("change", function() {
+                //     // Revalidate the field when an option is chosen
+                //     validator.revalidateField('tags');
+                // });
 
+                // // Due date. For more info, please visit the official plugin site: https://flatpickr.js.org/
+                // var dueDate = $(form.querySelector('[name="due_date"]'));
+                // dueDate.flatpickr({
+                //     enableTime: true,
+                //     dateFormat: "d, M Y, H:i",
+                // });
+
+                // // Team assign. For more info, plase visit the official plugin site: https://select2.org/
+                // $(form.querySelector('[name="team_assign"]')).on('change', function() {
+                //     // Revalidate the field when an option is chosen
+                //     validator.revalidateField('team_assign');
+                // });
             }
 
             // Handle form validation and submittion
@@ -59,6 +91,27 @@
                     form, {
                         fields: {
                             //TODO XANLAR
+                            // first_name: {
+                            //     validators: {
+                            //         notEmpty: {
+                            //             message: "{{ __('admin::general.pages.users.list.first_name_is_required') }}"
+                            //         }
+                            //     }
+                            // },
+                            // last_name: {
+                            //     validators: {
+                            //         notEmpty: {
+                            //             message: "{{ __('admin::general.pages.users.list.last_name_is_required') }}"
+                            //         }
+                            //     }
+                            // },
+                            // email: {
+                            //     validators: {
+                            //         notEmpty: {
+                            //             message: "{{ __('admin::general.pages.users.list.email_is_required') }}"
+                            //         }
+                            //     }
+                            // },
                         },
                         plugins: {
                             trigger: new FormValidation.plugins.Trigger(),
@@ -83,25 +136,32 @@
                             if (status == 'Valid') {
                                 let formData = $("#kt_modal_new2_target_form")
                                     .serializeArray();
+                                //array have null or empty value remove it
+                                formData = formData.filter(function(item) {
+                                    return item.value != null && item.value !=
+                                        "" && item.value != '0';
+                                });
+
                                 console.log(formData);
 
                                 submitButton.setAttribute('data-kt-indicator', 'on');
                                 // Disable button to avoid multiple click
                                 submitButton.disabled = true;
 
-                                //ROUTE UPDATE
+                                //ROUTE UPDATE 
                                 let url =
                                     `{{ route('', ['id' => '-1']) }}`;
+                                console.log(url);
 
                                 let id = $("#kt_modal_new2_target_form")
                                     .find(
                                         "input[name='edit_id']").val();
-                                //
-                                // url = url.replace("-1", emergencycall_id);
+
+                                url = url.replace("-1", id);
 
                                 $.ajax({
                                     type: "PUT",
-                                    url: url.replace("-1", id),
+                                    url: url, // Update with your API endpoint
                                     data: formData,
                                     success: function(response) {
                                         // Handle the success response here
@@ -143,18 +203,6 @@
                     }
                 });
 
-                closeButton.addEventListener('click', function(e) {
-                    e.preventDefault();
-
-                    customSwal.cancelIt().then(function(result) {
-                        if (result.value) {
-                            form.reset(); // Reset form
-                            modal.hide(); // Hide modal
-                        } else if (result.dismiss === 'cancel') {
-                            customSwal.formCancel();
-                        }
-                    });
-                });
                 cancelButton.addEventListener('click', function(e) {
                     e.preventDefault();
 
@@ -180,9 +228,9 @@
                     }
 
                     modal = new bootstrap.Modal(modalEl);
+
                     form = document.querySelector('#kt_modal_new2_target_form');
                     submitButton = document.getElementById('kt_modal_new2_target_submit');
-                    closeButton = document.getElementById('kt_modal_new2_target_close');
                     cancelButton = document.getElementById('kt_modal_new2_target_cancel');
 
                     initForm();
